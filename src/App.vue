c31b877a5fffc794b7ffc87ef4a99675b9dd9807
niranjanblank <niranjanshah474@gmail.com>
2020-12-30 19:48:57 +0545

    Login page front end created

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+   <join-group/>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld';
+import JoinGroup from './components/JoinGroup'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    'join-group':JoinGroup
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/JoinGroup.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-app class="d-flex justify-center align-center blue lighten-5">
+       <!-- <div class="container-class"> -->
+           <v-flex class="d-flex justify-center align-center">
+                <v-card width="400px" class="light-blue lighten-2 white--text pa-2 pl-8 pr-8">
+                <v-card-title class="d-flex flex-column align-center justify-center"><v-icon size="100" color="white">mdi-wechat</v-icon>
+                <p class="title-font">Chat It!!!</p></v-card-title>
+               
+                <v-form class="mt-2">
+                    <v-text-field label="Enter Display Name" solo ></v-text-field>
+                    <v-text-field label="Enter Room ID" solo ></v-text-field>
+                    <v-content class="d-flex flex-column align-center justify-center">
+                    <v-btn depressed  elevation="3" class="green accent-4 white--text mb-4">Join Chat</v-btn>
+                    </v-content>
+                </v-form>
+                </v-card>
+                
+           </v-flex>
+          
+        <!-- </div> -->
+    </v-app>
+</template>
+<script>
+export default {
+    name:'JoinGroup'
+}
+</script>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Redressed&display=swap');
+.container-class{
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+}
+
+.title-font{
+    font-family: 'Redressed';
+    font-size:80px;
+}
+</style>
